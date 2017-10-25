dcfb9747254a8a14a379ba5654ed3234de3aeca2
Zarak Mahmud <zarak.mahmud@utoronto.ca>
2017-10-25 21:13:04 +0500

    Cycle through five main questions

--- receive_sms.py
+++ receive_sms.py
@@ -17,51 +17,38 @@ def survey():
     }
 
     main_counter = session.get('main_counter', 0)
-    sub_counter = session.get('sub_counter', 0)
-    is_sub_question = session.get('sub_question', False)
+    # sub_counter = session.get('sub_counter', 0)
+    # is_sub_question = session.get('sub_question', False)
 
     question_list = [
             "Have you generated any new ideas?",
+            # "Type in the name of the idea and the seed stage",
             "Have you killed any ideas?",
             # "Type in the name of the idea",
             # "Type in the reason for killing the idea",
-            # "Any ideas on hold now?",
+            "Any ideas on hold now?",
             # "What ideas is on hold?",
-            # "Any name changes this week?",
+            "Any name changes this week?",
             # "From what old name to what new name?",
-            # "Any ideas advance stages or go backwards?",
+            "Any ideas advance stages or go backwards?"
             # "Which idea? And from what stage to what stage?"
             ]
 
-    sub_questions = [
-            ["Type in the name of the idea and the seed stage"],
-            ["Type in the name of the idea", "Type in the reason for killing the idea"],
-            ]
-
-    from_number = request.values.get('From', None)
-    current_question = question_list[main_counter]
     body = request.values.get('Body').lower()
+    if 'reset' in body:
+        main_counter = 0
+    current_question = question_list[main_counter]
 
     resp = MessagingResponse()
-    resp.message(current_question)
 
-    if 'reset' in body:
-        main_counter = sub_counter = 0
-
-    if is_sub_question == False:
-        if body == 'y' or 'yes' in body:
-            resp.message(question_list[main_counter])
-            is_sub_question = True
-        elif body == 'n' or 'no' in body:
-            main_counter += 1
-            resp.message(question_list[counter])
-            is_sub_question = False
-    else:
-        pass
-
+    if body == 'y' or 'yes' in body:
+        resp.message("You responded with yes, thanks!\n"
+                "(You may respond with N to move on to the next question)")
+    elif body == 'n' or 'no' in body:
+        main_counter += 1
+        resp.message(question_list[main_counter])
 
     session['main_counter'] = main_counter
-    session['sub_question'] = is_sub_question
 
     return str(resp)
 
